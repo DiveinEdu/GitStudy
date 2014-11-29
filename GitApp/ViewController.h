f9d44a1d09cdadbb0865fe4d2a8dfcd871755c07
apple <apple@lishan.local>
2014-11-29 21:02:53 +0800

    init

---
+++ GitApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitApp
+//
+//  Created by apple on 14/11/29.
+//  Copyright (c) 2014年 戴维营教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitApp
+//
+//  Created by apple on 14/11/29.
+//  Copyright (c) 2014年 戴维营教育. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
