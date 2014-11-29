d18e30290ea17a501a8c9a315a97baae3bcb8a47
apple <apple@lishan.local>
2014-11-29 21:05:09 +0800

    添加打印信息

--- GitApp/ViewController.m
+++ GitApp/ViewController.m
@@ -14,6 +14,11 @@
 
 @implementation ViewController
 
+- (void)print
+{
+    NSLog(@"这是一个打印信息");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
