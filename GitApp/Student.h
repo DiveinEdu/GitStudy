8598f1dbdd288c54c501ed5749bf6e539db6d1e6
apple <apple@lishan.lan>
2014-11-29 21:29:35 +0800

    添加Student类

---
+++ GitApp/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitApp
+//
+//  Created by apple on 14/11/29.
+//  Copyright (c) 2014年 戴维营教育. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
